c973ccf15121e8c47ec9c3d9f21fcc804f4c6809
donghoLee <45010017+WorldBestProgrammer@users.noreply.github.com>
2024-01-24 08:00:05 +0900

    [Silver III] Title: 배열 복원하기, Time: 1236 ms, Memory: 54552 KB -BaekjoonHub

--- 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -23,53 +23,71 @@ public class Main {
             }
         }
 
+      
         int[][] A = new int[H][W];
-        for (int i = 0; i < H; i++) {
-        	for (int j = 0; j < W; j++) {
-        		A[i][j] = -1;
-        	}
-        }
-        for (int i = 0; i < X; i++) {
-        	for (int j = 0; j < W; j++) {
-        		A[i][j] = B[i][j];
-        	}
-        }
-        
-        for (int i = X; i < H; i++) {
-        	for (int j = 0; j < Y; j++) {
-        		A[i][j] = B[i][j];
-        	}
-        }
-        
-        
-        for (int i = X; i < H; i++) {
-        	for (int j = W; j < W+Y; j++) {
-        		A[i-(X)][j-(Y)] = B[i][j];
-        	}
-        }
-        
-        for (int i = H; i < H + X; i++) {
-        	for (int j = Y; j < W; j++) {
-        		A[i-(X)][j-(Y)] = B[i][j];
-        	}
-        }
-        
-        for (int i = X; i < H; i++) {
-        	for (int j = Y; j < W; j++) {
-        		if (A[i][j] == -1) {
-        			A[i][j] = B[i][j] - A[i-X][j-Y];
-        		} else {
-        			A[i-X][j-Y] = B[i][j] - A[i][j];
-        		}
-        		
-        	}
-        }
-        
-        for (int i =0; i < H; i++) {
-        	for (int j = 0; j < W; j++) {
-        		System.out.print(A[i][j] + " ");
-        	}
-        	System.out.println();
-        }
+        initRectangle(A, B, H, W, X, Y);
+        findOriginalValueFromNotMovingRectangle(A, B, H, W, X, Y);
+        findOriginalValueFromMovingRectangle(A, B, H, W, X, Y);
+        findOriginalValueFromTwoRectangles(A, B, H, W, X, Y);
+        displayOriginalRectangle(A, B, H, W, X, Y);
+
     }
+
+    private static void initRectangle(int[][] A, int[][] B, int H, int W, int X, int Y) {
+      for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+          A[i][j] = -1;
+        }
+      }
+    }
+  
+    private static void findOriginalValueFromNotMovingRectangle(int[][] A, int[][] B, int H, int W, int X, int Y) {
+      for (int i = 0; i < X; i++) {
+        for (int j = 0; j < W; j++) {
+          A[i][j] = B[i][j];
+        }
+      }
+
+      for (int i = X; i < H; i++) {
+        for (int j = 0; j < Y; j++) {
+          A[i][j] = B[i][j];
+        }
+      }
+    }
+
+  private static void findOriginalValueFromMovingRectangle(int[][] A, int[][] B, int H, int W, int X, int Y) {
+    for (int i = X; i < H; i++) {
+      for (int j = W; j < W+Y; j++) {
+        A[i-(X)][j-(Y)] = B[i][j];
+      }
+    }
+
+    for (int i = H; i < H + X; i++) {
+      for (int j = Y; j < W; j++) {
+        A[i-(X)][j-(Y)] = B[i][j];
+      }
+    }
+  }
+
+  private static void findOriginalValueFromTwoRectangles(int[][] A, int[][] B, int H, int W, int X, int Y) {
+    for (int i = X; i < H; i++) {
+      for (int j = Y; j < W; j++) {
+        if (A[i][j] == -1) {
+          A[i][j] = B[i][j] - A[i-X][j-Y];
+        } else {
+          A[i-X][j-Y] = B[i][j] - A[i][j];
+        }
+
+      }
+    }
+  }
+
+  private static void displayOriginalRectangle(int[][] A, int[][] B, int H, int W, int X, int Y) {
+    for (int i =0; i < H; i++) {
+      for (int j = 0; j < W; j++) {
+        System.out.print(A[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 }
