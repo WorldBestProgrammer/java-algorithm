9b9a3844171fe8d3c97c8e0f5579d9c4a876a845
donghoLee <45010017+WorldBestProgrammer@users.noreply.github.com>
2024-01-29 21:28:12 +0900

    [Silver I] Title: 행렬, Time: 80 ms, Memory: 12044 KB -BaekjoonHub

--- 백준/Silver/1080. 행렬/행렬.java
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -15,6 +17,7 @@ class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
@@ -47,12 +50,14 @@ class Main {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 if (A[i][j] != B[i][j]) {
-                    System.out.println(-1);
+                    bw.write(String.valueOf(-1));
+                    bw.close();
                     return;
                 }
             }
         }
-        System.out.println(count);
+        bw.write(String.valueOf(count));
+        bw.close();
     }
 
     private static void flip(int[][] A, int x, int y) {
