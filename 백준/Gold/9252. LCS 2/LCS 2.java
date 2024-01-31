c369a82fce27360d7f6f3ef705e15de667231e05
donghoLee <45010017+WorldBestProgrammer@users.noreply.github.com>
2024-01-31 09:45:04 +0900

    [Gold IV] Title: LCS 2, Time: 108 ms, Memory: 15988 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,64 +1,85 @@
-import java.io.*;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-
-	static int[][] dp;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		String str1 = br.readLine();
-		String str2 = br.readLine();
+		char[] sequence1 = br.readLine().toCharArray();
+		char[] sequence2 = br.readLine().toCharArray();
+		int N = sequence1.length;
+		int M = sequence2.length;
 		
-		LCS2(str1,str2);
-		getLCSToString(str1, str1.length(), str2.length());
+		int[][] dp = new int[N + 1][M + 1];
 		
+		int lcs = findLCSLength(sequence1, sequence2, N, M, dp);
+		int x = N;
+		int y = M;
+//		display(dp);
+		StringBuilder sb = new StringBuilder();
 		
-		System.out.println(sb.toString());
+		while (x>0 && y>0) {
+			if (x == 0 || y == 0) {
+				break;
+			}
+			
+//				if (dp[x-1][y] > dp[x][y-1]) {
+//					x -= 1;
+//				} else if (dp[x-1][y] < dp[x][y-1]) {
+//					y -= 1;
+//				} else if (dp[x-1][y] == dp[x][y-1] && sequence1[x-1] == sequence2[y-1]) {
+//					sb.append(sequence1[x-1]);
+//					x -= 1;
+//					y -= 1;
+//				} else {
+//					x -= 1;
+//				}
+			
+			if (dp[x][y] == dp[x-1][y]) {
+				x--;
+			} else if (dp[x][y] == dp[x][y-1]) {
+				y--;
+			} else {
+				sb.append(sequence1[x-1]);
+				x--;
+				y--;
+			}
+			}
+			
+		System.out.println(lcs);
+		System.out.print(sb.reverse().toString());
+//		System.out.println(lcs);
+//		bw.write(lcs + '0');
+//		bw.newLine();
+//		if (lcs > 0) {
+//		bw.write(sb.reverse().toString());
+//		}
+//		bw.close();
 	}
-	
-	static void LCS2 (String str1, String str2) {
-		int n1 = str1.length();
-		int n2 = str2.length();
+	private static void display(int[][] graph) {
+		for (int i = 0 ; i < graph.length; i++) {
+			for (int j = 0; j < graph[0].length; j++) {
+				System.out.print(graph[i][j] + " ");
+			}
+			System.out.println();
+		}
 		
-		dp = new int[n1+1][n2+1];
-		int max =-1;
-		for(int i=1; i<n1+1; i++) {
-			for(int j=1; j<n2+1; j++) {
-				if(str1.charAt(i-1) == str2.charAt(j-1)) {
-					dp[i][j] = dp[i-1][j-1] +1;
-				}else {
+	}
+	private static int findLCSLength(char[] sequence1, char[] sequence2, int N, int M, int[][] dp) {
+		for (int i = 1; i <= N; i++) {	
+			for (int j = 1; j <= M; j++) {
+				if (sequence1[i-1] == sequence2[j-1]) {
+					dp[i][j] = dp[i-1][j-1] + 1;
+				} else {
 					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
 				}
 			}
 		}
 		
-		sb.append(dp[n1][n2] + "\n");
-	}
-	
-	static void getLCSToString(String str, int i, int j) {
-		Stack<Character> st = new Stack<>();
-		while(i>0 && j>0) {
-			
-			if(i == 0 || j ==0)break;
-			
-			if(dp[i][j] == dp[i-1][j]) {
-				i--;
-			}else if(dp[i][j] == dp[i][j-1]) {
-				j--;
-			}else {
-				st.push(str.charAt(i-1));
-				i--;
-				j--;
-			}
-			
-			
-		}
-		while(!st.isEmpty()) {
-			sb.append(st.pop());
-		}
-	
-		
+		return dp[N][M];
 	}
 }
