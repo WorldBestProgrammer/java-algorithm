14695895c57766e04e63e14382f0be1692cf0357
donghoLee <45010017+WorldBestProgrammer@users.noreply.github.com>
2024-02-21 09:44:22 +0900

    [Gold V] Title: 암호 만들기, Time: 84 ms, Memory: 12764 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int L;
+	static int C;
+	static char[] chars;
+	static char[] alphabet;
+	static String vowels = "aeiou";
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		alphabet = new char[C];
+		for (int i = 0; i < C; i++) {
+			alphabet[i] = st.nextToken().charAt(0);
+		}
+		
+		chars = new char[L];
+		Arrays.sort(alphabet);
+		comb(0, 0);
+		System.out.println(sb);
+		
+	}
+
+	private static void comb(int start, int depth) {
+		if (depth == L) {
+			int vowel = 0;
+			int consonant = 0;
+			for (int i = 0; i < L; i++) {
+				boolean flag = true;
+				for (char c : vowels.toCharArray()) {
+					if (c == chars[i]) {
+						vowel++;
+						flag = false;
+						break;
+					}
+				}
+				if (flag) {
+					consonant++;
+				}
+			}
+			if (vowel >= 1 && consonant >= 2) {
+				for (int i = 0; i < L; i++) {
+					sb.append(chars[i]);
+				}
+				sb.append("\n");
+			}
+			
+			return;
+		}
+		for (int i = start; i < C; i++) {
+			chars[depth++] = alphabet[i];
+			comb(i+1, depth);
+			depth--;
+		}
+	}
+}
