f98f9a71c5c4e4d5cdddda097f53cd8a35e85d9e
donghoLee <45010017+WorldBestProgrammer@users.noreply.github.com>
2024-04-23 22:01:58 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 224 ms, Memory: 14548 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -17,43 +17,19 @@ public class Main {
         }
 
         int[][] dp = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i == j) {
-                    continue;
-                }
-                dp[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        int[][][] size = new int[N][N][2];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < 2; j++) {
-                size[i][i][j] = matrix[i][j];
-            }
-        }
-
-        if (N == 1) {
-            return;
-        }
-        for (int i = 0; i < N - 1; i++) {
-            int m = i;
-            int n = i+1;
-            dp[m][n] = size[m][m][0] * size[m][m][1] * size[n][n][1];
-            size[m][n][0] = size[m][m][0];
-            size[m][n][1] = size[n][n][1];
-        }
-        for (int step = 2; step < N; step++) {
+        for (int step = 1; step < N; step++) {
             for (int i = 0; i < N - step; i++) {
                 int m = i;
                 int n = i + step;
+                dp[m][n] = Integer.MAX_VALUE;
                 for (int j = 0; j < n - m; j++) {
-                    dp[m][n] = Math.min(dp[m][n], dp[m][m + j] + dp[m + j + 1][n] + size[m][m + j][0] * size[m][m + j][1] * size[m + j +1][n][1]);
+                    dp[m][n] = Math.min(dp[m][n],
+                            dp[m][m + j] + dp[m + j + 1][n] + matrix[m][0] * matrix[m + j][1]
+                                    * matrix[n][1]);
                 }
-                size[m][n][0] = size[m][n-1][0];
-                size[m][n][1] = size[n][n][1];
             }
         }
 
-        System.out.println(dp[0][N-1]);
+        System.out.println(dp[0][N - 1]);
     }
 }
